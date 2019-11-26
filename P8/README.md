2f26579edf776d5701a31b5ca33684a1528ea68f
brian <alu0101013855@ull.edu.es>
2019-11-26 17:27:08 +0000

    subiendo consultas y actualizando readme

--- P8/README.md
+++ P8/README.md
@@ -1,5 +1,16 @@
 # Pruebas de las consultas realizadas
 
-### Consulta 1: Ver todos los campos de la tabla Familia
-![img1](https://user-images.githubusercontent.com/32529688/69544886-8cc65b00-0f88-11ea-8e82-8cd0b37733a2.png)
+### Consulta 1: Seleecionar todos los campos de la tabla Familia
+![img1](https://user-images.githubusercontent.com/32529688/69653607-662d2080-106b-11ea-893f-6752eb49c213.png)
 
+### Consulta 2: Códigos de los distintos laboratorios existentes
+![img2](https://user-images.githubusercontent.com/32529688/69653118-99bb7b00-106a-11ea-947a-39765657ea32.png)
+
+### Consulta 3: Nombre de los laboratorios con código 1
+![img3](https://user-images.githubusercontent.com/32529688/69653684-7fce6800-106b-11ea-94ca-43eb485999d5.png)
+
+### Consulta 4: Nombre y precio de los medicamentos que cuestan menos de 10€
+![img4](https://user-images.githubusercontent.com/32529688/69655854-5a435d80-106f-11ea-8ee9-de43f2b3a6a8.png)
+
+### Consulta 5: Nombre de los medicamentos cuyo importe es como mínimo de 5€
+![img5](https://user-images.githubusercontent.com/32529688/69657247-ccb53d00-1071-11ea-96eb-99447726f82f.png)
--- P8/consultas_farmacia.sql
+++ P8/consultas_farmacia.sql
@@ -1,24 +1,29 @@
--- -----------------------------------------------------
--- Consulta 1: Ver todos los campos de la tabla Familia
--- -----------------------------------------------------
+-- -------------------------------------------------------------
+-- Consulta 1: Seleccionar todos los campos de la tabla Familia
+-- -------------------------------------------------------------
 SELECT * FROM farmacia.FAMILIA;
 
--- -----------------------------------------------------
--- Consulta 2: ...
--- -----------------------------------------------------
-SELECT
+-- -------------------------------------------------------------
+-- Consulta 2: Códigos de los distintos laboratorios
+-- -------------------------------------------------------------
+SELECT Codigo_Lab FROM farmacia.LABORATORIO;
 
--- -----------------------------------------------------
--- Consulta 3: ...
--- -----------------------------------------------------
-SELECT
+-- -------------------------------------------------------------
+-- Consulta 3: Laboratorios cuyo código es "1"
+-- -------------------------------------------------------------
+SELECT Nombre FROM farmacia.LABORATORIO
+WHERE Codigo_Lab = 1;
 
--- -----------------------------------------------------
--- Consulta 4: ...
--- -----------------------------------------------------
-SELECT
+-- -------------------------------------------------------------
+-- Consulta 4: Medicamentos que cuestan menos de 10 euros
+-- -------------------------------------------------------------
+SELECT Nombre_Med, Precio FROM farmacia.MEDICAMENTOS
+WHERE Precio < 10;
 
--- -----------------------------------------------------
--- Consulta 5: ...
--- -----------------------------------------------------
-SELECT
+-- -------------------------------------------------------------
+-- Consulta 5: Medicamentos con un importe mínimo de 5 euros
+-- -------------------------------------------------------------
+SELECT Nombre_Med FROM farmacia.MEDICAMENTOS
+WHERE Codigo IN 
+(SELECT Codigo FROM farmacia.COMPRA_MEDICAMENTO
+WHERE Importe >= 5);
